4f58d4a6e56b8c3069fcba91efcc56e0dca8d869
osas david <osas@emaxcomm.com>
2026-03-26 04:47:41 -0500

    Revert: next.config back to original Supabase only

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: '**.supabase.co' },
       { protocol: 'https', hostname: '**.r2.cloudflarestorage.com' },
-      { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: 'images.pexels.com' },
-      { protocol: 'https', hostname: 'picsum.photos' },
     ],
   },
   experimental: {
