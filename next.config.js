6152e5886ed6e3f700f7f7929b4d4412ab2aed1e
osas david <osas@emaxcomm.com>
2026-03-25 23:15:50 -0500

    Fix: allow Unsplash and Pexels image domains

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: '**.supabase.co' },
       { protocol: 'https', hostname: '**.r2.cloudflarestorage.com' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'images.pexels.com' },
     ],
   },
   experimental: {
