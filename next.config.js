577854a62d386db43ab33b29c2b40708278192d5
osas david <osas@emaxcomm.com>
2026-03-26 04:40:34 -0500

    Fix: add picsum.photos to allowed image domains

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
       { protocol: 'https', hostname: '**.r2.cloudflarestorage.com' },
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'images.pexels.com' },
+      { protocol: 'https', hostname: 'picsum.photos' },
     ],
   },
   experimental: {
